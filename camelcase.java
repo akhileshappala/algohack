fa751689895a0e0eaf3487902c7464060d424a1c
saisiddhiakhilesh <akiakhilesh16@gmail.com>
2017-03-15 22:19:20 +0530

    Add files via upload

---
+++ camelcase.java
@@ -0,0 +1,18 @@
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        String s = in.next();
+        int count=0;
+        String[] strs = s.split("[A-Z]");
+        System.out.println(strs.length);
+        
+    }
+}
+
